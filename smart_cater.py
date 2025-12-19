b7b3a8b4809e9505b79d4cca40021487834172f0
navy356 <newatian@gmail.com>
2025-12-19 17:45:42 +0100

    docs: added images and nfr_explanation

--- smart_cater.py
+++ smart_cater.py
@@ -1,12 +1,15 @@
 from meal_db import MealDB
 from search_stratergy import *
 from menu import *
+import sys
 
 def main():
     """Main function to launch SmartCater"""
     MealDB.create_sample_meals_from_file()
     #Only run textual app if textual is available
     use_textual = True
+    if len (sys.argv)>1 and sys.argv[1] == "console":
+        use_textual = False
     if use_textual and TEXTUAL_AVAILABLE:
         run_textual_menu()
     else:
